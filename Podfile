6ca0cda7e15df3745d8dd7cc44832dc4d08c1a8b
ravibhavsar05 <ravib.dev@gmail.com>
2017-06-28 19:29:25 +0530

    need to update Firebase lib

--- Podfile
+++ Podfile
@@ -1,12 +1,17 @@
 # Uncomment the next line to define a global platform for your project
 # platform :ios, '9.0'
 
-target 'FirebaseTutorial' do
+target 'RBFirebaseLogin-Swift' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
 
-  # Pods for FirebaseTutorial
-  pod 'Firebase/Core'
+  # Pods for RBFirebaseLogin-Swift
+  pod 'Firebase/Storage'
+  pod 'Firebase/AdMob'
   pod 'Firebase/Auth'
-  
+  pod 'Firebase/Crash'
+  pod 'Firebase/Database'
+  pod 'Firebase/RemoteConfig'
+  pod 'Firebase/Core'
+  pod 'Firebase/Messaging'
 end
---
+++ RBFirebaseLogin-Swift/HomeViewController.swift
@@ -0,0 +1,33 @@
+
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func logOutAction(sender: AnyObject) {
+        if Auth.auth().currentUser != nil {
+            do {
+                try Auth.auth().signOut()
+                let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SignUp")
+                present(vc, animated: true, completion: nil)
+                
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
